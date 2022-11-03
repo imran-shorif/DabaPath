2fd25e4c05fd7b731ae895ce6926c88a99c01f9f
Raufun <raufun.nazin13@gmail.com>
2022-11-04 01:48:12 +0600

    buggy

--- app/src/main/java/com/example/dhuro/Blogs.java
+++ app/src/main/java/com/example/dhuro/Blogs.java
@@ -71,7 +71,7 @@ public class Blogs extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+            startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     @Override
