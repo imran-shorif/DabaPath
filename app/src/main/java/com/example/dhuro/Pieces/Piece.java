7bc75410e6128413326e42e4de4805fbae05e964
imran-shorif <shuvo.csedu2657@gmail.com>
2022-11-05 02:10:42 +0600

    added over the board chess

---
+++ app/src/main/java/com/example/dhuro/Pieces/Piece.java
@@ -0,0 +1,35 @@
+package com.example.dhuro.Pieces;
+
+import com.example.dhuro.Coordinates;
+import com.example.dhuro.Position;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DIPANSH KHANDELWAL on 03-06-2017
+ */
+
+public class Piece {
+
+    private boolean white;
+
+    Piece(boolean white) {
+        this.white = white;
+    }
+
+    public ArrayList<Coordinates> AllowedMoves(Coordinates coordinates , Position[][] board){
+        ArrayList<Coordinates> allowedMoves = new ArrayList<>();
+        Coordinates c;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                c = new Coordinates(i,j);
+                allowedMoves.add(c);
+            }
+        }
+        return allowedMoves;
+    }
+
+    public boolean isWhite() {
+        return white;
+    }
+}
